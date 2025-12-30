5a29c262add009d18ac58f212bb8f06914e5042d
simostrain <135849919+simostrain@users.noreply.github.com>
2025-12-30 10:55:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime, timezone
 
 # Test with these 4 coins
-TEST_SYMBOLS = ["BTCUSDT", "ETHUSDT", "BNBUSDT", "ADAUSDT"]
+TEST_SYMBOLS = ["MantaUSDT", "ETHUSDT", "BNBUSDT", "ADAUSDT"]
 BINANCE_API = "https://api.binance.com"
 
 session = requests.Session()
